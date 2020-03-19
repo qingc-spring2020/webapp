ab76762102c3def8813a239adc5ff30d98498108
qcwduyi <qing.c@husky.neu.edu>
2020-03-19 11:01:52 -0400

    5555

--- infrastructure/aws/codedeploy/start.sh
+++ infrastructure/aws/codedeploy/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /opt/tomcat/latest/webapps
-/usr/bin/nohup java -jar assignment3-0.0.1-SNAPSHOT.jar > /dev/project.log &
+/usr/bin/nohup java -jar assignment3-0.0.1-SNAPSHOT.jar > /dev/project.log 2>&1 &
 exit 0
