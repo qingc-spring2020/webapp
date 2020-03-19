51e8bf4fa11c7664df5087fff108fd530df94bba
qcwduyi <36652107+qcwduyi@users.noreply.github.com>
2020-03-19 16:58:41 -0400

    Update BillController.java

--- src/main/java/com/csye6225/assignment3/controller/BillController.java
+++ src/main/java/com/csye6225/assignment3/controller/BillController.java
@@ -35,7 +35,7 @@ public class BillController {
     @Autowired
     BillService billService;
 
-    @GetMapping("/v1/bills")
+    @GetMapping("/v2/bills")
     public Object getAllBillInfo(HttpServletRequest request,HttpServletResponse response) {
 
 
