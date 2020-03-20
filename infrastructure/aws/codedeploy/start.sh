636604ccadfc131f543dac612d13c32c9240c96d
qcwduyi <36652107+qcwduyi@users.noreply.github.com>
2020-03-19 20:13:15 -0400

    Update start.sh

--- infrastructure/aws/codedeploy/start.sh
+++ infrastructure/aws/codedeploy/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /home/ubuntu
-systemctl enable amazon-cloudwatch-agent.service
+sudo systemctl enable amazon-cloudwatch-agent.service
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:amazon-cloudwatch-agent.json -s
-sudo amazon-cloudwatch-agent.service restart
+sudo systemctl restart amazon-cloudwatch-agent.service
 cd /opt/tomcat/latest/webapps
 /usr/bin/nohup java -jar assignment3-0.0.1-SNAPSHOT.jar > /dev/project.log 2>&1 &
