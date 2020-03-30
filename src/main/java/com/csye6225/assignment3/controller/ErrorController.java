b546c33ced145510dcb8c57df9c8e8eefe4b4168
qcwduyi <qing.c@husky.neu.edu>
2020-03-30 11:05:05 -0400

    666666

--- src/main/java/com/csye6225/assignment3/controller/ErrorController.java
+++ src/main/java/com/csye6225/assignment3/controller/ErrorController.java
@@ -5,15 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     @RequestMapping("/error")
     @ResponseBody
-    public String handleError(HttpServletRequest request){
+    public String handleError(HttpServletResponse response){
         //获取statusCode:401,404,500
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        response.setStatus(200);
         return "welcome";
 
     }
